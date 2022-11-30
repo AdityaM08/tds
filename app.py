3cf47ea5471b7b561c4247bb5d8df162ea95912d
AdityaM08 <119511751+AdityaM08@users.noreply.github.com>
2022-11-30 21:27:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 st.write("TDS Assignment  8")
-p=st.integer_input("Enter first number")
-q=st.integer_input("Enter second number")
-r=st.integer_input("Enter third number") 
+p=st.number_input("Enter first number")
+q=st.number_input("Enter second number")
+r=st.number_input("Enter third number") 
 if (p >= q) and (p >= r):
    largest = p
 elif (q >= p) and (q >= r):
