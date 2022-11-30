01210829633a302b4746d9b674eb41ec1bfba1d3
AdityaM08 <119511751+AdityaM08@users.noreply.github.com>
2022-11-30 20:11:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ st.write("TDS Assignment  8")
 p=st.number_input("Enter first number")
 q=st.number_input("Enter second number")
 r=st.number_input("Enter third number") 
-if (p >= q) and (a >= r):
+if (p >= q) and (p >= r):
    largest = p
-elif (q >= p) and (b >= r):
+elif (q >= p) and (q >= r):
    largest = q
 else:
    largest = r
